c0a7b1d6af5b24a69eb64fb62b4d6dd5744598b2
Rosy <Rosy@192.168.0.114>
2014-09-02 20:50:11 -0300

    Commit com Classes quases prontas

---
+++ TCCMODIFICADO/src/java/TFD/Negocio/DAOGenerico.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TFD.Negocio;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Rosy
+ */
+public abstract class DAOGenerico{
+     private static EntityManager gerenciadorEntidade = null;
+    
+    /**
+     * Metodo criado com a finalidade de retornar o gerenciador de entidade.
+     * @return um objeto do tipo EntityManager, Reponsal por gerenciar o Banco de dados.
+     */
+    public static EntityManager getGerenciadorEntidade(){
+        if (gerenciadorEntidade == null || !gerenciadorEntidade.isOpen()){
+            gerenciadorEntidade = Persistence.createEntityManagerFactory("TCCMODIFICADOPU").createEntityManager();
+        }
+        return gerenciadorEntidade;
+    }
+}
